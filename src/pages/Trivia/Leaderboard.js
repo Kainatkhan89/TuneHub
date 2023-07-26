aec28b51d004a7271e58bce4f375ccbfe3bed5a6
VISHWA PANKAJBHAI PARMAR <vs623903@dal.ca>
2023-07-26 16:42:36 -0300

    Trivia Feature Completed
    Frontend( Normal and Mobile view) and Backend done

--- src/pages/Trivia/Leaderboard.js
+++ src/pages/Trivia/Leaderboard.js
@@ -1,12 +1,13 @@
 import { Box, Flex, Image, Text, VStack } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import LeaderboardSVG from '../../assets/learderboard.svg';
-// Leaderboard.js
 import { fetchLeaderboardData, submitScore } from '../../services/TriviaServices/LeaderboardServices';
 
 
 function Leaderboard() {
     const [leaderboardData, setLeaderboardData] = useState([]);
+    const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
 
     // Function to fetch leaderboard data from the backend
     useEffect(() => {
@@ -19,13 +20,51 @@ function Leaderboard() {
 
 
     return (
+        isMobile ?
+        <Flex justifyContent="space-around" minH="90vh" backgroundColor="#000C66" w="100%" flexDirection="column" >
+
+        <Flex w="60%"  alignSelf="center" m="24px">
+            <Image src={LeaderboardSVG} alt="Animated Woman with Power"  />
+        </Flex>
+        <Flex w="100%" flexDirection="column"  padding="20px" >
+           
+                <Text as="h1" fontSize="4xl" color="white"   mb="22px" fontWeight="medium"  alignSelf="center">
+                    Leaderboard
+                </Text>
+                {leaderboardData.map((data, index) => (
+                    <Box
+                        key={data._id}
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="space-between"
+                        width="100%"
+                        padding="10px"
+                        color="white"
+                        mb="24px"
+                        backgroundColor="#0a1c5a"
+                        borderRadius="8px"
+                        boxShadow="0 2px 4px rgba(0, 0, 0, 0.1)"
+                    >
+                        <Box fontSize="xl">
+                            {index + 1}.
+                        </Box>
+                        <Box fontWeight="medium">
+                            {data.name}
+                        </Box>
+                        <Text>{data.score} points</Text>
+                    </Box>
+                ))}
+            
+        </Flex>
+
+    </Flex>
+
+        :
         <Flex justifyContent="space-around" minH="90vh" backgroundColor="#000C66" w="100%" >
 
-            <Flex w="45%" >
-                <Image src={LeaderboardSVG} alt="Animated Woman with Power" boxSize="500px" />
-            </Flex>
-            <Flex w="45%">
-                <VStack spacing="4" align="start" color="white" order={[2, 2, 1]} mb="24px">
+            
+            <Flex w="45%" justifyContent="center">
+                <VStack spacing="4"   color="white" order={[2, 2, 1]} mb="24px" mt="32px">
                     <Text as="h1" fontSize="5xl" fontWeight="medium" color="white" mb="1">
                         Leaderboard
                     </Text>
@@ -52,6 +91,9 @@ function Leaderboard() {
                     ))}
                 </VStack>
             </Flex>
+            <Flex w="45%"  alignItems="center">
+            <Image src={LeaderboardSVG} alt="Animated Woman with Power" boxSize="500px" />
+        </Flex>
 
         </Flex>
     );
--- src/pages/Trivia/quiz.js
+++ src/pages/Trivia/quiz.js
@@ -1,8 +1,9 @@
-import { Flex, Heading, Button, Text, VStack, Image, useToast } from "@chakra-ui/react";
+import { Flex, Heading, Button, VStack, Image, useToast } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import fetchQuizQuestions from "../../services/TriviaServices/QuizService";
 import Trivia1SVG from "../../assets/trivia1.svg";
+import { useMediaQuery } from 'react-responsive';
 import { submitScore } from "../../services/TriviaServices/LeaderboardServices";
 
 function Quiz() {
@@ -14,6 +15,8 @@ function Quiz() {
   const toast = useToast();
   const navigate = useNavigate();
   const userID = 1;
+  const isMobile = useMediaQuery({ query: '(max-width: 1080px)' });
+
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -24,6 +27,7 @@ function Quiz() {
   }, []);
 
   const showToast = (message, isSuccess) => {
+   
     toast({
       title: message,
       status: isSuccess ? "success" : "warning",
@@ -32,8 +36,9 @@ function Quiz() {
       variant: isSuccess ? "subtle" : "solid",
       bg: isSuccess ? "#1db954" : "",
       color: isSuccess ? "white" : "",
-      position: "top",
+      position: isMobile ? "top" : "bottom",
     });
+    
   };
 
   const handleStartQuiz = () => {
@@ -63,11 +68,66 @@ function Quiz() {
   };
 
   return (
+    isMobile ?
+    questions ? (
+     
+      <Flex justifyContent="start" minH="90vh" backgroundColor="#000C66" w="100%" flexDirection="column" alignItems="center"  isMobile="true">
+        {!isQuizEnded ? isQuizStarted ? (
+          <VStack spacing="4" direction="column" align="start" color="white" mt="32px" mb="16px">
+            <Heading mb="32px" textAlign="center" size="lg"  >{questions[currentQuestionIndex].question}</Heading>
+            {["option1", "option2", "option3", "option4"].map((option, index) => (
+              <Button
+                key={index}
+                onClick={() => handleAnswerClick(questions[currentQuestionIndex][option])}
+                colorScheme="blue"
+                size="sm"
+                width="60%" // Adjust the width as needed
+                textAlign="center"
+                alignSelf="center"
+                whiteSpace="normal" // Allow text to wrap
+                wordWrap="break-word" // If needed, break words to fit within the width
+              
+                overflow="hidden" // Hide any overflow content
+               // Break the words if needed to fit within the Button
+                overflowWrap="break-word" // Wrap the content within words, if possible
+                padding="12px" // Adjust padding as needed
+              >
+                {questions[currentQuestionIndex][option]}
+              </Button>
+            ))}
+          </VStack>
+        ) : (
+          <>
+            <Image src={Trivia1SVG} alt="Animated Woman with Power" boxSize="25vh" m="16px"/>
+            <Heading  color="white" mb="24px" mt="64px" textAlign="center">
+              Welcome to Song Trivia!
+            </Heading>
+            <Button onClick={handleStartQuiz} colorScheme="blue" size="lg">
+              Start Trivia
+            </Button>
+          </>
+        ) : (
+          <Flex  justifyContent="center" flexDirection="column">
+            <Heading as="h1" size="xl" color="white" alignSelf="center" mt="128px" mb="32px">
+              Quiz completed! <br /> Your score: {score} / 5
+            </Heading>
+            <Button onClick={async () => {
+              navigateToLeaderboard();
+            }} colorScheme="blue" size="lg">
+              Leaderboard
+            </Button>
+          </Flex>
+        )}
+      </Flex>
+    ) : null
+
+    :
+
     questions ? (
       <Flex justifyContent="start" minH="90vh" backgroundColor="#000C66" w="100%" flexDirection="column" alignItems="center">
         {!isQuizEnded ? isQuizStarted ? (
           <VStack spacing="4" direction="column" align="start" color="white" mt="64px" mb="32px">
-            <Heading>{questions[currentQuestionIndex].question}</Heading>
+            <Heading mb="64px">{questions[currentQuestionIndex].question}</Heading>
             {["option1", "option2", "option3", "option4"].map((option, index) => (
               <Button
                 key={index}
@@ -75,6 +135,7 @@ function Quiz() {
                 colorScheme="blue"
                 size="md"
                 width="60%"
+                maxW="480px"
                 textAlign="center"
                 alignSelf="center"
               >
@@ -94,13 +155,12 @@ function Quiz() {
           </>
         ) : (
           <Flex direction="column" alignItems="center" width="100%">
-            <Heading as="h1" size="xl" color="white" mt="64px" mb="32px">
+            <Heading as="h1" size="xl" color="white" mt="248px" mb="32px">
               Quiz completed! <br /> Your score: {score} / 5
             </Heading>
             <Button onClick={async () => {
-              // When the user clicks the "Leaderboard" button,
-              // call the submitScore function to send the score to the backend
-              navigateToLeaderboard();
+                
+                navigateToLeaderboard();
             }} colorScheme="blue" size="lg">
               Leaderboard
             </Button>
